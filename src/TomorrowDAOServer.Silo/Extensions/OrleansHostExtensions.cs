676b451e6ec3a95fd38fa2c3010799c41f0ca31f
strongman6693 <strongman@tmrwdao.com>
2024-05-22 12:27:44 +0800

    feat: orleans deployment to k8s test

--- src/TomorrowDAOServer.Silo/Extensions/OrleansHostExtensions.cs
+++ src/TomorrowDAOServer.Silo/Extensions/OrleansHostExtensions.cs
@@ -2,66 +2,138 @@ using System.Net;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Orleans;
 using Orleans.Configuration;
 using Orleans.Hosting;
+using Orleans.Hosting.Kubernetes;
 using Orleans.Providers.MongoDB.Configuration;
 using Orleans.Statistics;
+using Serilog;
+
 namespace TomorrowDAOServer.Silo.Extensions;
 
 public static class OrleansHostExtensions
 {
-     public static IHostBuilder UseOrleansSnapshot(this IHostBuilder hostBuilder)
+    public static IHostBuilder UseOrleansSnapshot(this IHostBuilder hostBuilder)
     {
         return hostBuilder.UseOrleans((context, siloBuilder) =>
         {
             //Configure OrleansSnapshot
             var configSection = context.Configuration.GetSection("Orleans");
-            siloBuilder
-                .ConfigureEndpoints(advertisedIP:IPAddress.Parse(configSection.GetValue<string>("AdvertisedIP")),siloPort: configSection.GetValue<int>("SiloPort"), gatewayPort: configSection.GetValue<int>("GatewayPort"), listenOnAnyHostAddress: true)
-                .UseMongoDBClient(configSection.GetValue<string>("MongoDBClient"))
-                .UseMongoDBClustering(options =>
-                {
-                    options.DatabaseName = configSection.GetValue<string>("DataBase");;
-                    options.Strategy = MongoDBMembershipStrategy.SingleDocument;
-                })
-                .AddMongoDBGrainStorage("Default",(MongoDBGrainStorageOptions op) =>
-                {
-                    op.CollectionPrefix = "GrainStorage";
-                    op.DatabaseName = configSection.GetValue<string>("DataBase");
-                
-                    op.ConfigureJsonSerializerSettings = jsonSettings =>
-                    {
-                        // jsonSettings.ContractResolver = new PrivateSetterContractResolver();
-                        jsonSettings.NullValueHandling = NullValueHandling.Include;
-                        jsonSettings.DefaultValueHandling = DefaultValueHandling.Populate;
-                        jsonSettings.ObjectCreationHandling = ObjectCreationHandling.Replace;
-                    };
-                    
-                })
-                .UseMongoDBReminders(options =>
-                {
-                    options.DatabaseName = configSection.GetValue<string>("DataBase");
-                    options.CreateShardKeyForCosmos = false;
-                })
-                .Configure<ClusterOptions>(options =>
-                {
-                    options.ClusterId = configSection.GetValue<string>("ClusterId");
-                    options.ServiceId = configSection.GetValue<string>("ServiceId");
-                })
-               // .AddMemoryGrainStorage("PubSubStore")
-                .ConfigureApplicationParts(parts => parts.AddFromApplicationBaseDirectory())
-                .UseDashboard(options => {
-                    options.Username = configSection.GetValue<string>("DashboardUserName");
-                    options.Password = configSection.GetValue<string>("DashboardPassword");
-                    options.Host = "*";
-                    options.Port = configSection.GetValue<int>("DashboardPort");
-                    options.HostSelf = true;
-                    options.CounterUpdateIntervalMs = configSection.GetValue<int>("DashboardCounterUpdateIntervalMs");
-                })
-                .UseLinuxEnvironmentStatistics()
-                .ConfigureLogging(logging => { logging.SetMinimumLevel(LogLevel.Debug).AddConsole(); });
+            if (configSection.GetValue<bool>("IsRunningInKubernetes"))
+            {
+                Log.Logger.Debug("==Use kubernetes hosting...");
+                UseKubernetesHostClustering(siloBuilder, configSection);
+                Log.Logger.Debug("==Use kubernetes hosting end...");
+            }
+            else
+            {
+                Log.Logger.Debug("==Use docker hosting...");
+                UseDockerHostClustering(siloBuilder, configSection);
+                Log.Logger.Debug("==Use docker hosting end...");
+            }
         });
+    }
+
+    private static void UseKubernetesHostClustering(ISiloBuilder siloBuilder, IConfigurationSection configSection)
+    {
+        Log.Logger.Debug("==Configuration");
+        Log.Logger.Debug("==  POD_IP: {0}", Environment.GetEnvironmentVariable("POD_IP"));
+        Log.Logger.Debug("==  SiloPort: {0}", configSection.GetValue<int>("SiloPort"));
+        Log.Logger.Debug("==  GatewayPort: {0}", configSection.GetValue<int>("GatewayPort"));
+        Log.Logger.Debug("==  DatabaseName: {0}", configSection.GetValue<string>("DataBase"));
+        Log.Logger.Debug("==  ClusterId: {0}", Environment.GetEnvironmentVariable("ORLEANS_CLUSTER_ID"));
+        Log.Logger.Debug("==  ServiceId: {0}", Environment.GetEnvironmentVariable("ORLEANS_SERVICE_ID"));
+        Log.Logger.Debug("==Configuration");
+        siloBuilder.UseKubernetesHosting()
+            .ConfigureEndpoints(advertisedIP: IPAddress.Parse(Environment.GetEnvironmentVariable("POD_IP") ?? string.Empty),
+                siloPort: configSection.GetValue<int>("SiloPort"),
+                gatewayPort: configSection.GetValue<int>("GatewayPort"), listenOnAnyHostAddress: true)
+            .UseMongoDBClient(configSection.GetValue<string>("MongoDBClient"))
+            .UseMongoDBClustering(options =>
+            {
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.Strategy = MongoDBMembershipStrategy.SingleDocument;
+            })
+            .AddMongoDBGrainStorage("Default", (MongoDBGrainStorageOptions op) =>
+            {
+                op.CollectionPrefix = "GrainStorage";
+                op.DatabaseName = configSection.GetValue<string>("DataBase");
+                op.ConfigureJsonSerializerSettings = jsonSettings =>
+                {
+                    // jsonSettings.ContractResolver = new PrivateSetterContractResolver();
+                    jsonSettings.NullValueHandling = NullValueHandling.Include;
+                    jsonSettings.DefaultValueHandling = DefaultValueHandling.Populate;
+                    jsonSettings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+                };
+            })
+            .UseMongoDBReminders(options =>
+            {
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.CreateShardKeyForCosmos = false;
+            })
+            .Configure<ClusterOptions>(options =>
+            {
+                options.ClusterId = Environment.GetEnvironmentVariable("ORLEANS_CLUSTER_ID");
+                options.ServiceId = Environment.GetEnvironmentVariable("ORLEANS_SERVICE_ID");
+            })
+            .ConfigureApplicationParts(parts => parts.AddFromApplicationBaseDirectory())
+            .ConfigureLogging(logging => { logging.SetMinimumLevel(LogLevel.Debug).AddConsole(); });
+    }
+
+    private static void ConfigureOptions(OptionsBuilder<KubernetesHostingOptions> optionsBuilder)
+    {
+        Log.Logger.Debug("== builder kubernetes hosting options");
+    }
+
+    private static void UseDockerHostClustering(ISiloBuilder siloBuilder, IConfigurationSection configSection)
+    {
+        siloBuilder
+            .ConfigureEndpoints(advertisedIP: IPAddress.Parse(configSection.GetValue<string>("AdvertisedIP")),
+                siloPort: configSection.GetValue<int>("SiloPort"),
+                gatewayPort: configSection.GetValue<int>("GatewayPort"), listenOnAnyHostAddress: true)
+            .UseMongoDBClient(configSection.GetValue<string>("MongoDBClient"))
+            .UseMongoDBClustering(options =>
+            {
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.Strategy = MongoDBMembershipStrategy.SingleDocument;
+            })
+            .AddMongoDBGrainStorage("Default", (MongoDBGrainStorageOptions op) =>
+            {
+                op.CollectionPrefix = "GrainStorage";
+                op.DatabaseName = configSection.GetValue<string>("DataBase");
+                op.ConfigureJsonSerializerSettings = jsonSettings =>
+                {
+                    // jsonSettings.ContractResolver = new PrivateSetterContractResolver();
+                    jsonSettings.NullValueHandling = NullValueHandling.Include;
+                    jsonSettings.DefaultValueHandling = DefaultValueHandling.Populate;
+                    jsonSettings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+                };
+            })
+            .UseMongoDBReminders(options =>
+            {
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.CreateShardKeyForCosmos = false;
+            })
+            .Configure<ClusterOptions>(options =>
+            {
+                options.ClusterId = configSection.GetValue<string>("ClusterId");
+                options.ServiceId = configSection.GetValue<string>("ServiceId");
+            })
+            // .AddMemoryGrainStorage("PubSubStore")
+            .ConfigureApplicationParts(parts => parts.AddFromApplicationBaseDirectory())
+            .UseDashboard(options =>
+            {
+                options.Username = configSection.GetValue<string>("DashboardUserName");
+                options.Password = configSection.GetValue<string>("DashboardPassword");
+                options.Host = "*";
+                options.Port = configSection.GetValue<int>("DashboardPort");
+                options.HostSelf = true;
+                options.CounterUpdateIntervalMs = configSection.GetValue<int>("DashboardCounterUpdateIntervalMs");
+            })
+            .UseLinuxEnvironmentStatistics()
+            .ConfigureLogging(logging => { logging.SetMinimumLevel(LogLevel.Debug).AddConsole(); });
     }
 }
