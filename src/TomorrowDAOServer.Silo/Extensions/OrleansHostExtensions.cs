666863bd3dfa486fe602361d51b14abd469224e1
strongman6693 <strongman@tmrwdao.com>
2024-06-05 20:46:49 +0800

    feat: Orleans Test

--- src/TomorrowDAOServer.Silo/Extensions/OrleansHostExtensions.cs
+++ src/TomorrowDAOServer.Silo/Extensions/OrleansHostExtensions.cs
@@ -2,12 +2,10 @@ using System.Net;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Orleans;
 using Orleans.Configuration;
 using Orleans.Hosting;
-using Orleans.Hosting.Kubernetes;
 using Orleans.Providers.MongoDB.Configuration;
 using Orleans.Statistics;
 using Serilog;
@@ -18,26 +16,94 @@ public static class OrleansHostExtensions
 {
     public static IHostBuilder UseOrleansSnapshot(this IHostBuilder hostBuilder)
     {
-        Log.Logger.Warning("==UseOrleansSnapshot...");
         return hostBuilder.UseOrleans((context, siloBuilder) =>
         {
             //Configure OrleansSnapshot
             var configSection = context.Configuration.GetSection("Orleans");
-            Log.Logger.Warning("==Orleans.IsRunningInKubernetes={0}", configSection.GetValue<bool>("IsRunningInKubernetes"));
-            if (configSection.GetValue<bool>("IsRunningInKubernetes"))
+            
+            
+            Log.Logger.Warning("==  POD_IP: {0}", Environment.GetEnvironmentVariable("POD_IP"));
+            Log.Logger.Warning("==  SiloPort: {0}", configSection.GetValue<int>("SiloPort"));
+            Log.Logger.Warning("==  GatewayPort: {0}", configSection.GetValue<int>("GatewayPort"));
+            Log.Logger.Warning("==  DatabaseName: {0}", configSection.GetValue<string>("DataBase"));
+            Log.Logger.Warning("==  ClusterId: {0}", Environment.GetEnvironmentVariable("ORLEANS_CLUSTER_ID"));
+            Log.Logger.Warning("==  ServiceId: {0}", Environment.GetEnvironmentVariable("ORLEANS_SERVICE_ID"));
+
+            var IsRunningInKubernetes = configSection.GetValue<bool>("IsRunningInKubernetes");
+            var advertisedIP = IsRunningInKubernetes ?  Environment.GetEnvironmentVariable("POD_IP") :configSection.GetValue<string>("AdvertisedIP");
+            var clusterId = IsRunningInKubernetes ? Environment.GetEnvironmentVariable("ORLEANS_CLUSTER_ID") : configSection.GetValue<string>("ClusterId");
+            var serviceId = IsRunningInKubernetes ? Environment.GetEnvironmentVariable("ORLEANS_SERVICE_ID") : configSection.GetValue<string>("ServiceId");
+
+            siloBuilder
+            .ConfigureEndpoints(advertisedIP: IPAddress.Parse(advertisedIP),
+                siloPort: configSection.GetValue<int>("SiloPort"),
+                gatewayPort: configSection.GetValue<int>("GatewayPort"), listenOnAnyHostAddress: true)
+            .UseMongoDBClient(configSection.GetValue<string>("MongoDBClient"))
+            .UseMongoDBClustering(options =>
             {
-                Log.Logger.Warning("==Use kubernetes hosting...");
-                UseKubernetesHostClustering(siloBuilder, configSection);
-                Log.Logger.Warning("==Use kubernetes hosting end...");
-            }
-            else
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.Strategy = MongoDBMembershipStrategy.SingleDocument;
+            })
+            .AddMongoDBGrainStorage("Default", (MongoDBGrainStorageOptions op) =>
             {
-                Log.Logger.Warning("==Use docker hosting...");
-                UseDockerHostClustering(siloBuilder, configSection);
-                Log.Logger.Warning("==Use docker hosting end...");
-            }
+                op.CollectionPrefix = "GrainStorage";
+                op.DatabaseName = configSection.GetValue<string>("DataBase");
+                op.ConfigureJsonSerializerSettings = jsonSettings =>
+                {
+                    // jsonSettings.ContractResolver = new PrivateSetterContractResolver();
+                    jsonSettings.NullValueHandling = NullValueHandling.Include;
+                    jsonSettings.DefaultValueHandling = DefaultValueHandling.Populate;
+                    jsonSettings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+                };
+            })
+            .UseMongoDBReminders(options =>
+            {
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.CreateShardKeyForCosmos = false;
+            })
+            .Configure<ClusterOptions>(options =>
+            {
+                options.ClusterId = clusterId;
+                options.ServiceId = serviceId;
+            })
+            // .AddMemoryGrainStorage("PubSubStore")
+            .ConfigureApplicationParts(parts => parts.AddFromApplicationBaseDirectory())
+            .UseDashboard(options =>
+            {
+                options.Username = configSection.GetValue<string>("DashboardUserName");
+                options.Password = configSection.GetValue<string>("DashboardPassword");
+                options.Host = "*";
+                options.Port = configSection.GetValue<int>("DashboardPort");
+                options.HostSelf = true;
+                options.CounterUpdateIntervalMs = configSection.GetValue<int>("DashboardCounterUpdateIntervalMs");
+            })
+            .UseLinuxEnvironmentStatistics()
+            .ConfigureLogging(logging => { logging.SetMinimumLevel(LogLevel.Debug).AddConsole(); });
         });
     }
+    
+    // public static IHostBuilder UseOrleansSnapshot(this IHostBuilder hostBuilder)
+    // {
+    //     Log.Logger.Warning("==UseOrleansSnapshot...");
+    //     return hostBuilder.UseOrleans((context, siloBuilder) =>
+    //     {
+    //         //Configure OrleansSnapshot
+    //         var configSection = context.Configuration.GetSection("Orleans");
+    //         Log.Logger.Warning("==Orleans.IsRunningInKubernetes={0}", configSection.GetValue<bool>("IsRunningInKubernetes"));
+    //         if (configSection.GetValue<bool>("IsRunningInKubernetes"))
+    //         {
+    //             Log.Logger.Warning("==Use kubernetes hosting...");
+    //             UseKubernetesHostClustering(siloBuilder, configSection);
+    //             Log.Logger.Warning("==Use kubernetes hosting end...");
+    //         }
+    //         else
+    //         {
+    //             Log.Logger.Warning("==Use docker hosting...");
+    //             UseDockerHostClustering(siloBuilder, configSection);
+    //             Log.Logger.Warning("==Use docker hosting end...");
+    //         }
+    //     });
+    // }
 
     private static void UseKubernetesHostClustering(ISiloBuilder siloBuilder, IConfigurationSection configSection)
     {
@@ -85,10 +151,10 @@ public static class OrleansHostExtensions
             .ConfigureLogging(logging => { logging.SetMinimumLevel(LogLevel.Debug).AddConsole(); });
     }
 
-    private static void ConfigureOptions(OptionsBuilder<KubernetesHostingOptions> optionsBuilder)
-    {
-        Log.Logger.Warning("== builder kubernetes hosting options");
-    }
+    // private static void ConfigureOptions(OptionsBuilder<KubernetesHostingOptions> optionsBuilder)
+    // {
+    //     Log.Logger.Warning("== builder kubernetes hosting options");
+    // }
 
     private static void UseDockerHostClustering(ISiloBuilder siloBuilder, IConfigurationSection configSection)
     {
