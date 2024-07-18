20b6d38a7f87bda59d331f9a8c5d3c7ee2106763
strongman6693 <strongman@tmrwdao.com>
2024-07-18 17:21:01 +0800

    feat: ElectionProvider interface unit test

--- src/TomorrowDAOServer.Application/Election/Provider/ElectionProvider.cs
+++ src/TomorrowDAOServer.Application/Election/Provider/ElectionProvider.cs
@@ -11,15 +11,12 @@ using TomorrowDAOServer.Common.GraphQL;
 using TomorrowDAOServer.Common.Provider;
 using TomorrowDAOServer.Election.Dto;
 using TomorrowDAOServer.Election.Index;
-using Volo.Abp.Application.Dtos;
 using Volo.Abp.DependencyInjection;
 
 namespace TomorrowDAOServer.Election.Provider;
 
 public interface IElectionProvider
 {
-    Task<PagedResultDto<IndexerElection>> GetHighCouncilListAsync(GetHighCouncilListInput input);
-
     Task<ElectionPageResultDto<ElectionCandidateElectedDto>> GetCandidateElectedRecordsAsync(
         GetCandidateElectedRecordsInput input);
 
@@ -52,47 +49,6 @@ public class ElectionProvider : IElectionProvider, ISingletonDependency
         _logger = logger;
         _graphQlProvider = graphQlProvider;
         _highCouncilManagedDaoRepository = highCouncilManagedDaoRepository;
-    }
-
-    [Obsolete]
-    public async Task<PagedResultDto<IndexerElection>> GetHighCouncilListAsync(GetHighCouncilListInput input)
-    {
-        try
-        {
-            var graphQlResponse = await _graphQlHelper.QueryAsync<IndexerElectionResult>(new GraphQLRequest
-            {
-                Query =
-                    @"query($sorting:String,$skipCount:Int!,$maxMaxResultCount:Int!,$chainId:String,$DAOId:String,$highCouncilType:String,$termNumber:Long!){
-            data:getHighCouncilListAsync(input: {sorting:$sorting,skipCount:$skipCount,maxMaxResultCount:$maxMaxResultCount,chainId:$chainId,DAOId:$DAOId,highCouncilType:$highCouncilType,termNumber:$termNumber})
-                dataList{
-                    id,chainId,DAOId,termNumber,highCouncilType,address,votesAmount,stakeAmount
-                }
-                ,totalCount
-            }",
-                Variables = new
-                {
-                    input.Sorting,
-                    input.SkipCount,
-                    input.MaxResultCount,
-                    input.ChainId,
-                    input.DAOId,
-                    input.HighCouncilType,
-                    input.TermNumber
-                }
-            });
-            return new PagedResultDto<IndexerElection>
-            {
-                TotalCount = graphQlResponse.TotalCount,
-                Items = graphQlResponse.DataList
-            };
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "GetHighCouncilListAsyncError chainId {chainId} DAOId {DAOId} termNumber{termNumber}",
-                input.ChainId, input.DAOId, input.TermNumber);
-        }
-
-        return new PagedResultDto<IndexerElection>(0, new List<IndexerElection>());
     }
 
     public async Task<ElectionPageResultDto<ElectionCandidateElectedDto>> GetCandidateElectedRecordsAsync(
