7061d48b0b48c1ab090651e7c11d254df13e2750
strongman6693 <strongman@tmrwdao.com>
2024-08-05 11:12:41 +0800

    feat: performance testing

--- src/TomorrowDAOServer.HttpApi/Controllers/DaoController.cs
+++ src/TomorrowDAOServer.HttpApi/Controllers/DaoController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TomorrowDAOServer.Common.Dtos;
 using TomorrowDAOServer.DAO;
 using TomorrowDAOServer.DAO.Dtos;
@@ -19,46 +21,83 @@ namespace TomorrowDAOServer.Controllers;
 public class DaoController
 {
     private readonly IDAOAppService _daoAppService;
+    private readonly ILogger<DaoController> _logger;
 
-    public DaoController(IDAOAppService daoAppService)
+    public DaoController(IDAOAppService daoAppService, ILogger<DaoController> logger)
     {
         _daoAppService = daoAppService;
+        _logger = logger;
     }
     
     [HttpGet("dao-info")]
     public async Task<DAOInfoDto> GetDAOByIdAsync(GetDAOInfoInput input)
     {
-        return await _daoAppService.GetDAOByIdAsync(input);
+        var sw = Stopwatch.StartNew();
+        var dto = await _daoAppService.GetDAOByIdAsync(input);
+        
+        sw.Stop();
+        _logger.LogInformation("DaoController GetDAOByIdAsync duration:{0}", sw.ElapsedMilliseconds);
+        
+        return dto;
     }
 
     [HttpGet("member-list")]
     public async Task<PageResultDto<MemberDto>> GetMemberListAsync(GetMemberListInput listInput)
     {
-        return await _daoAppService.GetMemberListAsync(listInput);
+        var sw = Stopwatch.StartNew();
+        
+        var result = await _daoAppService.GetMemberListAsync(listInput);
+        sw.Stop();
+        _logger.LogInformation("DaoController GetMemberListAsync duration:{0}", sw.ElapsedMilliseconds);
+        
+        return result;
+        
     }
     
     [HttpGet("is-member")]
     public async Task<bool> IsDaoMemberAsync(IsDaoMemberInput input)
     {
-        return await _daoAppService.IsDaoMemberAsync(input);
+        var sw = Stopwatch.StartNew();
+
+        var result = await _daoAppService.IsDaoMemberAsync(input);
+        
+        sw.Stop();
+        _logger.LogInformation("DaoController IsDaoMemberAsync duration:{0}", sw.ElapsedMilliseconds);
+        return result;
     }
 
     [HttpGet("dao-list")]
     public async Task<PagedResultDto<DAOListDto>> GetDAOListAsync(QueryDAOListInput request)
     {
-        return await _daoAppService.GetDAOListAsync(request);
+        var sw = Stopwatch.StartNew();
+        var result = await _daoAppService.GetDAOListAsync(request);
+        
+        sw.Stop();
+        _logger.LogInformation("DaoController GetDAOListAsync duration:{0}", sw.ElapsedMilliseconds);
+        return result;
     }
     
     [HttpGet("bp-list")]
     public async Task<List<string>> BPList([Required]string chainId)
     {
-        return await _daoAppService.GetBPList(chainId);
+        var sw = Stopwatch.StartNew();
+        
+        var bpList = await _daoAppService.GetBPList(chainId);
+        
+        sw.Stop();
+        _logger.LogInformation("DaoController BPList duration:{0}", sw.ElapsedMilliseconds);
+        return bpList;
     }
 
     [HttpGet("my-dao-list")]
     [Authorize]
     public async Task<List<MyDAOListDto>> MyDAOList(QueryMyDAOListInput input)
     {
-        return await _daoAppService.GetMyDAOListAsync(input);
+        var sw = Stopwatch.StartNew();
+        var result = await _daoAppService.GetMyDAOListAsync(input);
+        
+        sw.Stop();
+        _logger.LogInformation("DaoController MyDAOList duration:{0}", sw.ElapsedMilliseconds);
+        return result;
     }
 }
--- src/TomorrowDAOServer.HttpApi/Controllers/ProposalController.cs
+++ src/TomorrowDAOServer.HttpApi/Controllers/ProposalController.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TomorrowDAOServer.Proposal;
 using TomorrowDAOServer.Proposal.Dto;
 using Volo.Abp;
@@ -15,49 +17,89 @@ namespace TomorrowDAOServer.Controllers;
 public class ProposalController : AbpController
 {
     private readonly IProposalService _proposalService;
+    private readonly ILogger<ProposalController> _logger;
 
-    public ProposalController(IProposalService proposalService)
+    public ProposalController(IProposalService proposalService, ILogger<ProposalController> logger)
     {
         _proposalService = proposalService;
+        _logger = logger;
     }
 
     [HttpPost("list")]
     public async Task<ProposalPagedResultDto<ProposalDto>> QueryProposalListAsync(QueryProposalListInput input)
     {
-         return await _proposalService.QueryProposalListAsync(input);
+        var sw = Stopwatch.StartNew();
+
+        var result = await _proposalService.QueryProposalListAsync(input);
+        
+        sw.Stop();
+        _logger.LogInformation("ProposalController QueryProposalListAsync duration:{0}", sw.ElapsedMilliseconds);
+        
+        return result;
     }
     
     [HttpGet]
     [Route("detail")]
     public async Task<ProposalDetailDto> QueryProposalDetailAsync(QueryProposalDetailInput input)
     {
-        return await _proposalService.QueryProposalDetailAsync(input);
+        var sw = Stopwatch.StartNew();
+        
+        var result = await _proposalService.QueryProposalDetailAsync(input);
+        
+        sw.Stop();
+        _logger.LogInformation("ProposalController QueryProposalDetailAsync duration:{0}", sw.ElapsedMilliseconds);
+        
+        return result;
     }
     
     [HttpGet("my-info")]
     [Authorize]
     public async Task<MyProposalDto> QueryMyInfoAsync(QueryMyProposalInput input)
     {
-        return await _proposalService.QueryMyInfoAsync(input);
+        var sw = Stopwatch.StartNew();
+        
+        var result = await _proposalService.QueryMyInfoAsync(input);
+        
+        sw.Stop();
+        _logger.LogInformation("ProposalController QueryMyInfoAsync duration:{0}", sw.ElapsedMilliseconds);
+        
+        return result;
     }
     
     [HttpGet("vote-history")]
     [Authorize]
     public async Task<VoteHistoryDto> QueryVoteHistoryAsync(QueryVoteHistoryInput input)
     {
-        return await _proposalService.QueryVoteHistoryAsync(input);
+        var sw = Stopwatch.StartNew();
+        
+        var result = await _proposalService.QueryVoteHistoryAsync(input);
+        
+        sw.Stop();
+        _logger.LogInformation("ProposalController QueryVoteHistoryAsync duration:{0}", sw.ElapsedMilliseconds);
+        
+        return result;
     }
 
     [HttpGet("executable-list")]
     public async Task<ProposalPagedResultDto<ProposalBasicDto>> QueryExecutableProposalsAsync(QueryExecutableProposalsInput input)
     {
-        return await _proposalService.QueryExecutableProposalsAsync(input);
+        var sw = Stopwatch.StartNew();
+        
+        var result = await _proposalService.QueryExecutableProposalsAsync(input);
+        
+        sw.Stop();
+        _logger.LogInformation("ProposalController QueryExecutableProposalsAsync duration:{0}", sw.ElapsedMilliseconds);
+        
+        return result;
     }
     
     // TestUse
     [HttpGet("clear")]
     public async Task Clear(string chainId)
     {
+        var sw = Stopwatch.StartNew();
         await _proposalService.ClearData(chainId);
+        sw.Stop();
+        _logger.LogInformation("ProposalController Clear duration:{0}", sw.ElapsedMilliseconds);
     }
 }
