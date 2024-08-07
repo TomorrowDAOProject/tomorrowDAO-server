b281bd8f66e1da077b75fba2c61a0ab373264468
devgantmrwdao <devgan@tmrwdao.com>
2024-08-07 10:44:08 +0800

    feat: change log

--- src/TomorrowDAOServer.HttpApi.Host/Filter/LoggingFilter.cs
+++ src/TomorrowDAOServer.HttpApi.Host/Filter/LoggingFilter.cs
@@ -18,13 +18,11 @@ public class LoggingFilter : IActionFilter
     public void OnActionExecuting(ActionExecutingContext context)
     {
         _stopwatch = Stopwatch.StartNew();
-        _logger.LogInformation($"Starting request for {context.ActionDescriptor.DisplayName} at {DateTime.UtcNow}");
     }
 
     public void OnActionExecuted(ActionExecutedContext context)
     {
         _stopwatch.Stop();
-        _logger.LogInformation($"Ending request for {context.ActionDescriptor.DisplayName} at {DateTime.UtcNow}");
-        _logger.LogInformation($"Request for {context.ActionDescriptor.DisplayName} duration {_stopwatch.ElapsedMilliseconds} ms");
+        _logger.LogInformation($"RequestFor {context.ActionDescriptor.DisplayName} use {_stopwatch.ElapsedMilliseconds} ms");
     }
 }
