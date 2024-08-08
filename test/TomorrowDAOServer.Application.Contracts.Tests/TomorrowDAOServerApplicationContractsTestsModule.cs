93a0f5c99cb87d2e1b1c4b87c168f486b44a443f
strongman6693 <strongman@tmrwdao.com>
2024-08-08 14:44:40 +0800

    feat: ContractProvider unit test

---
+++ test/TomorrowDAOServer.Application.Contracts.Tests/TomorrowDAOServerApplicationContractsTestsModule.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.Auditing;
+using Volo.Abp.Authorization;
+using Volo.Abp.Autofac;
+using Volo.Abp.AutoMapper;
+using Volo.Abp.Caching;
+using Volo.Abp.Modularity;
+using Volo.Abp.ObjectMapping;
+
+namespace TomorrowDAOServer.Application.Contracts.Tests;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(AbpTestBaseModule),
+    typeof(AbpAuthorizationModule),
+    typeof(AbpCachingModule),
+    typeof(AbpAutoMapperModule),
+    typeof(AbpObjectMappingModule),
+    typeof(TomorrowDAOServerDomainModule),
+    typeof(TomorrowDAOServerDomainTestModule),
+    typeof(TomorrowDAOServerApplicationContractsModule)
+)]
+public class TomorrowDaoServerApplicationContractsTestsModule : AbpModule
+{
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpAuditingOptions>(options =>
+        {
+            options.IsEnabled = false;
+        });
+        
+        Configure<AbpAutoMapperOptions>(options => { options.AddMaps<TomorrowDaoServerApplicationContractsTestsModule>(); });
+        
+
+        context.Services.AddMemoryCache();
+        
+        base.ConfigureServices(context);
+    }
+
+}
