3e8fa68153a92702165ae57618d33ad8bc01a8a2
devgantmrwdao <devgan@tmrwdao.com>
2024-08-21 15:22:25 +0800

    feat: add fields

--- src/TomorrowDAOServer.Application.Contracts/Proposal/Dto/IndexerVoteHistoryDto.cs
+++ src/TomorrowDAOServer.Application.Contracts/Proposal/Dto/IndexerVoteHistoryDto.cs
@@ -18,6 +18,7 @@ public class IndexerVoteHistoryDto
     public string Decimals { get; set; } = "0";
     public string Symbol { get; set; } = string.Empty;
     public string Voter { get; set; } = string.Empty;
+    public bool ValidRankingVote { get; set; }
     public long Points { get; set; }
     public string VoteFor { get; set; } = string.Empty;
     public string Memo { get; set; } = string.Empty;
