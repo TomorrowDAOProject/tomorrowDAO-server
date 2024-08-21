6e098fec6a246e13ce7516cf0732c110aa1e8091
devgantmrwdao <devgan@tmrwdao.com>
2024-08-21 15:34:27 +0800

    feat: alias

--- src/TomorrowDAOServer.Application.Contracts/Proposal/Dto/IndexerVoteHistoryDto.cs
+++ src/TomorrowDAOServer.Application.Contracts/Proposal/Dto/IndexerVoteHistoryDto.cs
@@ -19,6 +19,7 @@ public class IndexerVoteHistoryDto
     public string Symbol { get; set; } = string.Empty;
     public string Voter { get; set; } = string.Empty;
     public bool ValidRankingVote { get; set; }
+    public string Alias { get; set; } = string.Empty;
     public long Points { get; set; }
     public string VoteFor { get; set; } = string.Empty;
     public string Memo { get; set; } = string.Empty;
