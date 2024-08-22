1d242a9a97f8be0255ece26c956a2ee3ce41c336
devgantmrwdao <devgan@tmrwdao.com>
2024-08-22 16:01:49 +0800

    feat: order

--- src/TomorrowDAOServer.Application.Contracts/Common/CommonConstant.cs
+++ src/TomorrowDAOServer.Application.Contracts/Common/CommonConstant.cs
@@ -41,6 +41,7 @@ public static class CommonConstant
     //LogEvent
     public const string VoteEventVoted = "Voted";
     public const string MemoPattern = @"##GameRanking\s*:\s*\{([^}]+)\}";
+    public const string DescriptionBegin = "##GameRanking:";
     public const string DayFormatString = "yyyyMMdd";
     public const long TenMinutes = 10 * 60 * 1000;
     public const long OneDay = 24 * 60 * 60 * 1000;
