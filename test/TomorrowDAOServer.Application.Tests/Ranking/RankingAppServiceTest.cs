538e4b0d6622c1f18a5959588fe4e935e988617b
devgantmrwdao <devgan@tmrwdao.com>
2024-08-22 12:55:03 +0800

    feat: ut

--- test/TomorrowDAOServer.Application.Tests/Ranking/RankingAppServiceTest.cs
+++ test/TomorrowDAOServer.Application.Tests/Ranking/RankingAppServiceTest.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using TomorrowDAOServer.Proposal.Index;
+using TomorrowDAOServer.Ranking.Dto;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -33,5 +34,17 @@ public partial class RankingAppServiceTest : TomorrowDaoServerApplicationTestBas
                 ProposalId = ProposalId1, ProposalDescription = "##GameRanking:crypto-bot"
             }
         });
+    }
+
+    [Fact]
+    public async Task GetDefaultRankingProposalAsyncTest()
+    {
+        await _rankingAppService.GetDefaultRankingProposalAsync(ChainIdTDVV);
+    }
+    
+    [Fact]
+    public async Task GetRankingProposalListAsyncTest()
+    {
+        await _rankingAppService.GetRankingProposalListAsync(new GetRankingListInput{ChainId = ChainIdTDVV});
     }
 }
