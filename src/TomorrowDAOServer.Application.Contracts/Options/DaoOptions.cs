59d6a9aad517d6b51936bbaabafa4968b4c84a5a
devgantmrwdao <devgan@tmrwdao.com>
2024-08-27 14:32:45 +0800

    feat: dao list sort

--- src/TomorrowDAOServer.Application.Contracts/Options/DaoOptions.cs
+++ src/TomorrowDAOServer.Application.Contracts/Options/DaoOptions.cs
@@ -1,9 +1,27 @@
 using System.Collections.Generic;
+using System.Linq;
+using TomorrowDAOServer.Enums;
 
 namespace TomorrowDAOServer.Options;
 
 public class DaoOptions
 {
     public ISet<string> FilteredDaoNames { get; set; } = new HashSet<string>();
-    public List<string> TopDaoNames { get; set; } = new();
+    public Dictionary<VerifiedType, List<string>> TopDaoNames { get; set; } = new();
+    
+    public List<string> GetTopDaoNames()
+    {
+        return TopDaoNames
+            .OrderBy(kv => kv.Key)
+            .SelectMany(kv => kv.Value) 
+            .Distinct() 
+            .ToList();
+    }
+
+    public Dictionary<string, VerifiedType> GetVerifiedTypeDic()
+    {
+        return TopDaoNames
+            .SelectMany(kv => kv.Value.Select(value => new { Key = value, Value = kv.Key }))
+            .ToDictionary(pair => pair.Key, pair => pair.Value);
+    }
 }
