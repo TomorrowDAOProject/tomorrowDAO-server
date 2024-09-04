81e848cf717e080c65aa420888a6923c490a1453
devgantmrwdao <devgan@tmrwdao.com>
2024-09-04 17:20:32 +0800

    feat: test func

--- src/TomorrowDAOServer.HttpApi/Controllers/TestController.cs
+++ src/TomorrowDAOServer.HttpApi/Controllers/TestController.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TomorrowDAOServer.Ranking.Provider;
 using Volo.Abp;
+using Volo.Abp.Caching;
 
 namespace TomorrowDAOServer.Controllers;
 
@@ -12,15 +13,30 @@ namespace TomorrowDAOServer.Controllers;
 public class TestController
 {
     private IRankingAppPointsRedisProvider _rankingAppPointsRedisProvider;
+    private readonly IDistributedCache<string> _distributedCache;
 
-    public TestController(IRankingAppPointsRedisProvider rankingAppPointsRedisProvider)
+    public TestController(IRankingAppPointsRedisProvider rankingAppPointsRedisProvider, 
+        IDistributedCache<string> distributedCache)
     {
         _rankingAppPointsRedisProvider = rankingAppPointsRedisProvider;
+        _distributedCache = distributedCache;
     }
     
     [HttpGet("redis-value")]
     public async Task<string> GetRedisValueAsync(string key)
     {
         return await _rankingAppPointsRedisProvider.GetAsync(key);
+    }
+    
+    [HttpGet("redis-value-distributed-cache")]
+    public async Task<string> GetRedisValueDistributedCacheAsync(string key)
+    {
+        return await _distributedCache.GetAsync(key);
+    }
+    
+    [HttpGet("default-proposal-id")]
+    public async Task<string> GetDefaultRankingProposalIdAsync(string chainId)
+    {
+        return await _rankingAppPointsRedisProvider.GetDefaultRankingProposalIdAsync(chainId);
     }
 }
