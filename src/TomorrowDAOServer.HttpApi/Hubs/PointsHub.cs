4133ab96f0ac51f2370b3b8141fa914d43bd0b2e
devgantmrwdao <devgan@tmrwdao.com>
2024-09-05 01:12:11 +0800

    feat: log

--- src/TomorrowDAOServer.HttpApi/Hubs/PointsHub.cs
+++ src/TomorrowDAOServer.HttpApi/Hubs/PointsHub.cs
@@ -103,7 +103,7 @@ public class PointsHub : AbpHub
 
     private bool IsEqual(IReadOnlyCollection<RankingAppPointsBaseDto> currentPoints)
     {
-        _logger.LogInformation("IsEqual currentPoints {currentPoints}, _pointsCache {_pointsCache}", currentPoints, _pointsCache);
+        _logger.LogInformation("IsEqual currentPoints {currentPoints}, _pointsCache {_pointsCache}", JsonConvert.SerializeObject(currentPoints), JsonConvert.SerializeObject(_pointsCache));
         return currentPoints.Count == _pointsCache.Count
                && !currentPoints.Except(_pointsCache, new AllFieldsEqualComparer<RankingAppPointsBaseDto>()).Any();
     }
