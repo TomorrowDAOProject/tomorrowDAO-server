fddd9bfb54a6bb873568c9c14f7e266f7e0bbdaa
devgantmrwdao <devgan@tmrwdao.com>
2024-10-09 19:45:03 +0800

    feat: add [Required]

--- src/TomorrowDAOServer.Application.Contracts/Discover/Dto/GetDiscoverAppListInput.cs
+++ src/TomorrowDAOServer.Application.Contracts/Discover/Dto/GetDiscoverAppListInput.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TomorrowDAOServer.Discover.Dto;
 
 public class GetDiscoverAppListInput
 {
-    public string ChainId { get; set; }
-    public string Category { get; set; }
+    [Required] public string ChainId { get; set; }
+    [Required] public string Category { get; set; }
     public int SkipCount { get; set; } = 0;
     public int MaxResultCount { get; set; } = 8;
 }
