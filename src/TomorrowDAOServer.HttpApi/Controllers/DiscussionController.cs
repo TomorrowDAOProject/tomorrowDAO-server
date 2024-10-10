cd9392d1daf88c378e852a8c3d0c77188fb530ae
devgantmrwdao <devgan@tmrwdao.com>
2024-10-10 11:37:56 +0800

    feat: remove un use

--- src/TomorrowDAOServer.HttpApi/Controllers/DiscussionController.cs
+++ src/TomorrowDAOServer.HttpApi/Controllers/DiscussionController.cs
@@ -35,9 +35,9 @@ public class DiscussionController
         return await _discussionService.GetCommentListAsync(input);
     }
     
-    [HttpGet("comment-building")]
-    public async Task<CommentBuildingDto> GetCommentBuildingAsync(GetCommentBuildingInput input)
-    {
-        return await _discussionService.GetCommentBuildingAsync(input);
-    }
+    // [HttpGet("comment-building")]
+    // public async Task<CommentBuildingDto> GetCommentBuildingAsync(GetCommentBuildingInput input)
+    // {
+    //     return await _discussionService.GetCommentBuildingAsync(input);
+    // }
 }
