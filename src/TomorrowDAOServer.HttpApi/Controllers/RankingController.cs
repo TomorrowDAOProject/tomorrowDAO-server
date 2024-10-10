a5addaad6982a6fc8553ab5296be572633040d72
devgantmrwdao <devgan@tmrwdao.com>
2024-10-10 14:31:52 +0800

    feat: use multi get

--- src/TomorrowDAOServer.HttpApi/Controllers/RankingController.cs
+++ src/TomorrowDAOServer.HttpApi/Controllers/RankingController.cs
@@ -72,8 +72,8 @@ public class RankingController : AbpController
     
     [HttpGet("activity-result")]
     [Authorize]
-    public async Task GetRankingActivityResultAsync(string chainId, string proposalId)
+    public async Task GetRankingActivityResultAsync(string chainId, string proposalId, int count)
     {
-        await _rankingAppService.GetRankingActivityResultAsync(chainId, proposalId);
+        await _rankingAppService.GetRankingActivityResultAsync(chainId, proposalId, count);
     }
 }
