914c69440b1aeb5ad97f0acae256168b99c0e94f
devgantmrwdao <devgan@tmrwdao.com>
2024-10-24 14:38:36 +0800

    feat: change records

--- src/TomorrowDAOServer.Application.Contracts/ResourceToken/Dtos/GetRecordsInput.cs
+++ src/TomorrowDAOServer.Application.Contracts/ResourceToken/Dtos/GetRecordsInput.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace TomorrowDAOServer.ResourceToken.Dtos;
 
-public class GetRecordsInput
+public class GetRecordsInput : IValidatableObject
 {
     public int Limit { get; set; }
     public int Page { get; set; }
     public string Order { get; set; }
     public string Address { get; set; }
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Limit < 0)
+        {
+            yield return new ValidationResult($"Limit invalid: less than 0.");
+        }
+        if (Page < 0)
+        {
+            yield return new ValidationResult($"Page invalid: less than 0.");
+        }
+    }
 }
