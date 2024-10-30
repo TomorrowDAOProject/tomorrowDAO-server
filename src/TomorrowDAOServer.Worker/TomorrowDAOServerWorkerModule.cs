7fb69d618bc772fb4f5809961a79426ece5e1152
devgantmrwdao <devgan@tmrwdao.com>
2024-10-30 14:21:51 +0800

    feat: tmp

--- src/TomorrowDAOServer.Worker/TomorrowDAOServerWorkerModule.cs
+++ src/TomorrowDAOServer.Worker/TomorrowDAOServerWorkerModule.cs
@@ -15,21 +15,21 @@ namespace TomorrowDAOServer.Worker
         public override void OnApplicationInitialization(ApplicationInitializationContext context)
         {
             var backgroundWorkerManger = context.ServiceProvider.GetRequiredService<IBackgroundWorkerManager>();
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<BPInfoUpdateWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ProposalSyncWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<DAOSyncWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<VoteRecordSyncWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<VoteWithdrawSyncWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ProposalNewUpdateWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<HighCouncilMemberSyncWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<TokenPriceUpdateWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ProposalNumUpdateWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ReferralSyncWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<UserBalanceSyncWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ReferralTopInviterGenerateWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ProposalRedisUpdateWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<TopProposalGenerateWorker>());
-            backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<TelegramAppsSyncWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<BPInfoUpdateWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ProposalSyncWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<DAOSyncWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<VoteRecordSyncWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<VoteWithdrawSyncWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ProposalNewUpdateWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<HighCouncilMemberSyncWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<TokenPriceUpdateWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ProposalNumUpdateWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ReferralSyncWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<UserBalanceSyncWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ReferralTopInviterGenerateWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<ProposalRedisUpdateWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<TopProposalGenerateWorker>());
+            // backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<TelegramAppsSyncWorker>());
             backgroundWorkerManger.AddAsync(context.ServiceProvider.GetService<FindminiAppsSyncWorker>());
         }
     }
