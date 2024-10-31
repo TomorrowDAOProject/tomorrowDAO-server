180fa79810b3623f264fe3de2474cf5f4c7c1404
devgantmrwdao <devgan@tmrwdao.com>
2024-10-31 15:14:48 +0800

    feat: fix image

--- src/TomorrowDAOServer.Application/MapperBase.cs
+++ src/TomorrowDAOServer.Application/MapperBase.cs
@@ -50,4 +50,37 @@ public class MapperBase : Profile
 
         return list.Select(x => x.ToString()).ToList();
     }
+
+    protected static string MapIcon(SourceType sourceType, string icon)
+    {
+        if (sourceType != SourceType.FindMini)
+        {
+            return icon;
+        }
+
+        if (icon == null)
+        {
+            return string.Empty;
+        }
+
+        return icon.StartsWith("/") ? CommonConstant.FindminiUrlPrefix + icon : icon;
+    }
+    
+    protected static List<string> MapScreenshots(SourceType sourceType, List<string> screenshots)
+    {
+        if (sourceType != SourceType.FindMini)
+        {
+            return screenshots;
+        }
+        
+        var res = new List<string>();
+        if (screenshots == null || screenshots.IsNullOrEmpty())
+        {
+            return res;
+        }
+
+        res.AddRange(screenshots.Select(screenshot => screenshot.StartsWith("/") ? CommonConstant.FindminiUrlPrefix + screenshot : screenshot));
+        
+        return res;
+    }
 }
