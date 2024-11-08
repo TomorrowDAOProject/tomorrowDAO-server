b79a8923f135fca02c69c6539a8f3a8bd4ab0ac5
devgantmrwdao <devgan@tmrwdao.com>
2024-11-08 12:04:20 +0800

    feat: fix

--- src/TomorrowDAOServer.Application/Proposal/Provider/ProposalProvider.cs
+++ src/TomorrowDAOServer.Application/Proposal/Provider/ProposalProvider.cs
@@ -235,7 +235,8 @@ public class ProposalProvider : IProposalProvider, ISingletonDependency
         }
         QueryContainer Filter(QueryContainerDescriptor<ProposalIndex> f) => f.Bool(b => b.Must(mustQuery));
 
-        return await _proposalIndexRepository.GetSortListAsync(Filter, sortFunc: _ => new SortDescriptor<ProposalIndex>().Descending(index => index.DeployTime),
+        return await _proposalIndexRepository.GetSortListAsync(Filter, sortFunc: _ => new SortDescriptor<ProposalIndex>()
+                .Descending(a => a.ActiveEndTime > DateTime.UtcNow).Descending(index => index.DeployTime),
             skip: skipCount, limit: maxResultCount);
     }
 
@@ -471,15 +472,5 @@ public class ProposalProvider : IProposalProvider, ISingletonDependency
             q => q.Term(i =>
                 i.Field(f => f.ProposalStatus).Value(proposalStatus))
         };
-    }
-    
-    private static Func<SortDescriptor<ProposalIndex>, IPromise<IList<ISort>>> GetCustomSortDescriptor()
-    {
-        var nowUtc = DateTime.UtcNow;
-        var sortDescriptor = new SortDescriptor<ProposalIndex>();
-        sortDescriptor
-            .Descending(a => a.ActiveEndTime > nowUtc) 
-            .Descending(a => a.DeployTime);            
-        return _ => sortDescriptor;
     }
 }
