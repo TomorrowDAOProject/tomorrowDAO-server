263d380c282fb59f20cf5847c60523cd068bf7e1
devgantmrwdao <devgan@tmrwdao.com>
2024-11-18 14:46:15 +0800

    feat: tmp

--- src/TomorrowDAOServer.Application/Spider/FindminiAppsSyncDataService.cs
+++ src/TomorrowDAOServer.Application/Spider/FindminiAppsSyncDataService.cs
@@ -35,11 +35,11 @@ public class FindminiAppsSyncDataService : ScheduleSyncDataService
     public override async Task<long> SyncIndexerRecordsAsync(string chainId, long lastEndHeight, long newIndexHeight)
     {
         var hour =  _telegramOptions.CurrentValue.FindminiSpiderTime;
-        if (TimeHelper.IsTimestampToday(lastEndHeight) || DateTime.UtcNow.Hour != hour)
-        {
-            _logger.LogInformation("FindminiNoNeedToSync");
-            return lastEndHeight;
-        }
+        // if (TimeHelper.IsTimestampToday(lastEndHeight) || DateTime.UtcNow.Hour != hour)
+        // {
+        //     _logger.LogInformation("FindminiNoNeedToSync");
+        //     return lastEndHeight;
+        // }
         _logger.LogInformation("FindminiNeedToSyncStart");
         var categoryList = _telegramOptions.CurrentValue.FindMiniCategoryList;
         foreach (var url in categoryList.Select(category => CommonConstant.FindminiCategoryPrefix + category + "/"))
