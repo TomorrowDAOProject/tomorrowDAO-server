949fedb3ae9d9ac64bb325edd29799809b6c8a0e
devgantmrwdao <devgan@tmrwdao.com>
2024-12-05 19:49:33 +0800

    feat: using

--- src/TomorrowDAOServer.Application/User/UserService.cs
+++ src/TomorrowDAOServer.Application/User/UserService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AElf.ExceptionHandler;
 using Aetherlink.PriceServer.Common;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
