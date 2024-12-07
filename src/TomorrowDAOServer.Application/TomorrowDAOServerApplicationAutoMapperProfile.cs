e8241fc058729b4cd36ff21ee0f2f04f04259972
strongman6693 <strongman@tmrwdao.com>
2024-12-07 10:54:00 +0800

    feat: fix homepage issue

--- src/TomorrowDAOServer.Application/TomorrowDAOServerApplicationAutoMapperProfile.cs
+++ src/TomorrowDAOServer.Application/TomorrowDAOServerApplicationAutoMapperProfile.cs
@@ -293,6 +293,7 @@ public class TomorrowDAOServerApplicationAutoMapperProfile : MapperBase
             .ForMember(des => des.InviteeCaHash, opt
                 => opt.MapFrom(source => source.CaHash))
             ;
+        CreateMap<TelegramAppIndex, RankingAppDetailDto>();
 
         CreateMap<ReferralCodeInfo, ReferralLinkCodeIndex>()
             .ForMember(des => des.InviterCaHash, opt
