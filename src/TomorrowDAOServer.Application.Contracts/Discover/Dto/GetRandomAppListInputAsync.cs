f813872e4b01f3d249a30370c31b371df87b481a
devgantmrwdao <devgan@tmrwdao.com>
2024-12-08 20:19:20 +0800

    feat: space

--- src/TomorrowDAOServer.Application.Contracts/Discover/Dto/GetRandomAppListInputAsync.cs
+++ src/TomorrowDAOServer.Application.Contracts/Discover/Dto/GetRandomAppListInputAsync.cs
@@ -7,6 +7,6 @@ public class GetRandomAppListInputAsync
 {
     [Required] public string ChainId { get; set; }
     public string Category { get; set; } = string.Empty;
-    public int MaxResultCount { get; set; } = 16;
-    public List<string> Aliases { get; set; }
+    public int MaxResultCount { get; set; } = 8;
+    public List<string> Aliases { get; set; } = new();
 }
